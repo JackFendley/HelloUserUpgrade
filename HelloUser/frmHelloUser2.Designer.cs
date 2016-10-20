6bc58dd2665231c9572a2bb6b9452b482acced93
JackFendley <jackfendley18@gmail.com>
2016-10-20 10:03:42 -0500

    Upgraded Hello User
    Upgraded version of the hello user assignment

---
+++ HelloUser/frmHelloUser2.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace HelloUser
+{
+    partial class frmHelloUser2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisplay_Click = new System.Windows.Forms.Button();
+            this.btnDisplay_Click2 = new System.Windows.Forms.Button();
+            this.btnDisplay_Click3 = new System.Windows.Forms.Button();
+            this.txtCoffeeInput = new System.Windows.Forms.TextBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.lblCoffeeQuestion = new System.Windows.Forms.Label();
+            this.btnGreeting = new System.Windows.Forms.Button();
+            this.lblDisplay2 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtSecondNum = new System.Windows.Forms.TextBox();
+            this.txtFirstNum = new System.Windows.Forms.TextBox();
+            this.lblFirstNum = new System.Windows.Forms.Label();
+            this.lblDisplay3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDisplay_Click
+            // 
+            this.btnDisplay_Click.Location = new System.Drawing.Point(111, 331);
+            this.btnDisplay_Click.Name = "btnDisplay_Click";
+            this.btnDisplay_Click.Size = new System.Drawing.Size(163, 23);
+            this.btnDisplay_Click.TabIndex = 0;
+            this.btnDisplay_Click.Text = "Display Info";
+            this.btnDisplay_Click.UseVisualStyleBackColor = true;
+            this.btnDisplay_Click.Click += new System.EventHandler(this.btnDisplay_Click_Click);
+            // 
+            // btnDisplay_Click2
+            // 
+            this.btnDisplay_Click2.Location = new System.Drawing.Point(280, 331);
+            this.btnDisplay_Click2.Name = "btnDisplay_Click2";
+            this.btnDisplay_Click2.Size = new System.Drawing.Size(163, 23);
+            this.btnDisplay_Click2.TabIndex = 1;
+            this.btnDisplay_Click2.Text = "Total Price";
+            this.btnDisplay_Click2.UseVisualStyleBackColor = true;
+            this.btnDisplay_Click2.Click += new System.EventHandler(this.btnDisplay_Click2_Click);
+            // 
+            // btnDisplay_Click3
+            // 
+            this.btnDisplay_Click3.Location = new System.Drawing.Point(111, 359);
+            this.btnDisplay_Click3.Name = "btnDisplay_Click3";
+            this.btnDisplay_Click3.Size = new System.Drawing.Size(163, 23);
+            this.btnDisplay_Click3.TabIndex = 2;
+            this.btnDisplay_Click3.Text = "More Info";
+            this.btnDisplay_Click3.UseVisualStyleBackColor = true;
+            this.btnDisplay_Click3.Click += new System.EventHandler(this.btnDisplay_Click3_Click);
+            // 
+            // txtCoffeeInput
+            // 
+            this.txtCoffeeInput.Location = new System.Drawing.Point(280, 304);
+            this.txtCoffeeInput.Name = "txtCoffeeInput";
+            this.txtCoffeeInput.Size = new System.Drawing.Size(163, 20);
+            this.txtCoffeeInput.TabIndex = 3;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.Location = new System.Drawing.Point(111, 47);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(163, 223);
+            this.lblDisplay.TabIndex = 4;
+            this.lblDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCoffeeQuestion
+            // 
+            this.lblCoffeeQuestion.AutoSize = true;
+            this.lblCoffeeQuestion.Location = new System.Drawing.Point(277, 278);
+            this.lblCoffeeQuestion.Name = "lblCoffeeQuestion";
+            this.lblCoffeeQuestion.Size = new System.Drawing.Size(166, 13);
+            this.lblCoffeeQuestion.TabIndex = 5;
+            this.lblCoffeeQuestion.Text = "How Many Coffee\'s do you want?";
+            this.lblCoffeeQuestion.Click += new System.EventHandler(this.lblCoffeeQuestion_Click);
+            // 
+            // btnGreeting
+            // 
+            this.btnGreeting.Location = new System.Drawing.Point(111, 302);
+            this.btnGreeting.Name = "btnGreeting";
+            this.btnGreeting.Size = new System.Drawing.Size(163, 23);
+            this.btnGreeting.TabIndex = 6;
+            this.btnGreeting.Text = "Greeting";
+            this.btnGreeting.UseVisualStyleBackColor = true;
+            this.btnGreeting.Click += new System.EventHandler(this.btnGreeting_Click);
+            // 
+            // lblDisplay2
+            // 
+            this.lblDisplay2.Location = new System.Drawing.Point(452, 47);
+            this.lblDisplay2.Name = "lblDisplay2";
+            this.lblDisplay2.Size = new System.Drawing.Size(163, 231);
+            this.lblDisplay2.TabIndex = 7;
+            this.lblDisplay2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(449, 359);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(160, 23);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtSecondNum
+            // 
+            this.txtSecondNum.Location = new System.Drawing.Point(449, 333);
+            this.txtSecondNum.Name = "txtSecondNum";
+            this.txtSecondNum.Size = new System.Drawing.Size(160, 20);
+            this.txtSecondNum.TabIndex = 9;
+            // 
+            // txtFirstNum
+            // 
+            this.txtFirstNum.Location = new System.Drawing.Point(449, 304);
+            this.txtFirstNum.Name = "txtFirstNum";
+            this.txtFirstNum.Size = new System.Drawing.Size(160, 20);
+            this.txtFirstNum.TabIndex = 10;
+            // 
+            // lblFirstNum
+            // 
+            this.lblFirstNum.AutoSize = true;
+            this.lblFirstNum.Location = new System.Drawing.Point(470, 278);
+            this.lblFirstNum.Name = "lblFirstNum";
+            this.lblFirstNum.Size = new System.Drawing.Size(116, 13);
+            this.lblFirstNum.TabIndex = 11;
+            this.lblFirstNum.Text = "Please enter a number:";
+            // 
+            // lblDisplay3
+            // 
+            this.lblDisplay3.Location = new System.Drawing.Point(283, 47);
+            this.lblDisplay3.Name = "lblDisplay3";
+            this.lblDisplay3.Size = new System.Drawing.Size(160, 223);
+            this.lblDisplay3.TabIndex = 13;
+            this.lblDisplay3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmHelloUser2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 636);
+            this.Controls.Add(this.lblDisplay3);
+            this.Controls.Add(this.lblFirstNum);
+            this.Controls.Add(this.txtFirstNum);
+            this.Controls.Add(this.txtSecondNum);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblDisplay2);
+            this.Controls.Add(this.btnGreeting);
+            this.Controls.Add(this.lblCoffeeQuestion);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.txtCoffeeInput);
+            this.Controls.Add(this.btnDisplay_Click3);
+            this.Controls.Add(this.btnDisplay_Click2);
+            this.Controls.Add(this.btnDisplay_Click);
+            this.Name = "frmHelloUser2";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisplay_Click;
+        private System.Windows.Forms.Button btnDisplay_Click2;
+        private System.Windows.Forms.Button btnDisplay_Click3;
+        private System.Windows.Forms.TextBox txtCoffeeInput;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lblCoffeeQuestion;
+        private System.Windows.Forms.Button btnGreeting;
+        private System.Windows.Forms.Label lblDisplay2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtSecondNum;
+        private System.Windows.Forms.TextBox txtFirstNum;
+        private System.Windows.Forms.Label lblFirstNum;
+        private System.Windows.Forms.Label lblDisplay3;
+    }
+}
+
---
+++ HelloUser/frmHelloUser2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloUser
+{
+    public partial class frmHelloUser2 : Form
+    {
+        public frmHelloUser2()
+        {
+            InitializeComponent();
+        }
+
+        /**
+
+ *
+
+ * Name: Jack Fendley
+
+ * Teacher: Mr. Hardman
+
+ * Assignment #2, Program #1
+
+ * Date Last Modified: October 20th, 2016
+
+ *
+ */
+
+        private void btnDisplay_Click_Click(object sender, EventArgs e)
+        {
+            int myNumber = 55;
+
+            bool myBoolean = true;
+
+            char mycharacter = '?';
+
+            lblDisplay.Text = myNumber.ToString() +
+                "\n" + "Bill Murray" +
+                "\n" + "Movies" +
+                "\n" + myBoolean.ToString() + mycharacter.ToString();
+        }
+
+        private void btnDisplay_Click2_Click(object sender, EventArgs e)
+        {
+            const double PST_RATE = 0.08;
+            const double GST_RATE = 0.05;
+
+            double coffeePrice = 1.80;
+            int numCoffees;
+            double subtotal;
+            double taxes;
+            double finalTotal;
+
+            numCoffees = Convert.ToInt32(txtCoffeeInput.Text);
+            subtotal = numCoffees * coffeePrice;
+            taxes = subtotal * (PST_RATE + GST_RATE);
+            finalTotal = subtotal + taxes;
+
+            lblDisplay3.Text += "\n\n" + "PST is " + PST_RATE;
+            lblDisplay3.Text += "\n" + "GST is " + GST_RATE;
+            lblDisplay3.Text += "\n" + "Coffee costs $" + coffeePrice;
+            lblDisplay3.Text += "\n" + "Your total is $" + finalTotal;
+        }
+
+        private void btnDisplay_Click3_Click(object sender, EventArgs e)
+        {
+            string myString = "Bill Murray";
+
+            lblDisplay.Text += "\n" + myString.Substring(0, 4);
+            lblDisplay.Text += "\n" + myString.Substring(4);
+
+            lblDisplay.Text += "\n" + "Age: ";
+            lblDisplay.Text += +myString.IndexOf('u');
+            lblDisplay.Text += +myString.IndexOf('u');
+            lblDisplay.Text += "\n" + "Movies: ";
+            lblDisplay.Text += +myString.IndexOf('M');
+            lblDisplay.Text += +myString.IndexOf('M');
+        }
+
+        private void btnGreeting_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "Yo what's up my homeslice?";
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double FirstNum = Convert.ToDouble(txtFirstNum.Text);
+            double SecondNum = Convert.ToDouble(txtSecondNum.Text);
+
+            double sum = FirstNum + SecondNum;
+            double difference = FirstNum - SecondNum;
+            double product = FirstNum * SecondNum;
+            double quotient = FirstNum / SecondNum;
+            double mod = FirstNum % SecondNum;
+
+            double absOfFirst = Math.Abs(FirstNum);
+            double absOfSecond = Math.Abs(SecondNum);
+            double power = Math.Pow(FirstNum, SecondNum);
+            double root = Math.Pow(SecondNum, 1 / FirstNum);
+
+            lblDisplay2.Text = "Your sum is: " + sum;
+            lblDisplay2.Text += "\nYour difference is: " + difference;
+            lblDisplay2.Text += "\nYour product is: " + product;
+            lblDisplay2.Text += "\nYour quotient is: " + quotient;
+            lblDisplay2.Text += "\nYour modulus is: " + mod;
+            lblDisplay2.Text += "\nYour power is: " + power;
+            lblDisplay2.Text += "\nYour root is: " + root;
+            lblDisplay2.Text += "\nThe absolute value of the first number is: " + absOfFirst;
+            lblDisplay2.Text += "\nThe absolute value of the second number is: " + absOfSecond;
+        }
+
+        private void lblCoffeeQuestion_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
